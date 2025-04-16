1d5e628e327ab255bf6eb0cf710f16ce683b5171
DeepooBelief <101371088+DeepooBelief@users.noreply.github.com>
2025-04-16 23:14:54 +0800

    Update u_balance_control.h

--- 2.Firmware/MCU1/include/control/u_balance_control.h
+++ 2.Firmware/MCU1/include/control/u_balance_control.h
@@ -37,7 +37,11 @@ class UBalanceControl
         if (fabs(sensor.angle - lqr_param_.angle_offset) < fabs(angle_protection_threahold_) &&
             cube_is_upside_dowm_ == false)
         {
-            value = AxisLqr(sensor, lqr_param_);
+            // value = AxisLqr(sensor, lqr_param_);da
+            static float alpha = 0.0022f;
+            static float angle_offset = lqr_param_.angle_offset;
+            angle_offset = (1 - alpha) * angle_offset + alpha * sensor.angle;
+            value = lqr_param_.kp * (sensor.angle - angle_offset + 0.23f) + sensor.gyro * lqr_param_.kv + sensor.speed * lqr_param_.ks;
             u_auto_calibration_.Update(sensor.speed);
             is_balance_ = true;
         }
